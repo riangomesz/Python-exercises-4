2ae28b14aa2fa98c681773ecd430c0fc86625780
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-12-21 12:52:11 -0300

    Add files via upload

---
+++ cardio_rate.py
@@ -0,0 +1,7 @@
+print("Calculador de batimentos do Coração")
+bpm = 65
+idade = int(input("Digite sua idade: "))
+bpa = 65 * 525.600
+bpa = str(bpa)
+print(str("Seu coração bateu "+bpa+" vezes em toda sua vida"))
+print("Agradeça por estar vivo")
