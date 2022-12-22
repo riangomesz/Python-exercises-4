8a085719a34e43151954c111f545fe231d959d93
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-12-22 13:24:43 -0300

    Update pequisa_cep.py

--- pequisa_cep.py
+++ pequisa_cep.py
@@ -1,3 +1,7 @@
+# Antes de iniciar o desenvolvimento do código, fui no powershell e instalei o pacote 'requests'.
+# Após isso desenvolvi o programa que faz com que o usuário informe seu cep.
+# Depois ele imprime na tela os dados de sua casa. A partir de uma API web que busca "ceps" online.
+
 import requests
 
 cep = int(input("Digite seu cep: "))
