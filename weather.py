a3f46175fc178d612d5461094b79b2ce607f0d82
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-05-01 15:36:12 -0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+
+apiKey = 'b5647a2a3eaf4416830181957230105'
+city = 'Belo Horizonte'
+
+response = requests.get(f'https://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}')
+data = response.json()
+
+temperature = data['current']['temp_c']
+description = data['current']['condition']['text']
+
+print(f'Em {city}, a temperatura atual é de {temperature} graus Celsius e o tempo está {description}.')
